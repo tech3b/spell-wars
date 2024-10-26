0d2e4511b0b6b4633410cba9b5ab8eaa58780a02
l1cache <cache@bk.ru>
2024-10-27 00:18:14 +0200

    some unwraps removed

---
+++ client/src/message.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <tuple>
+#include <boost/asio.hpp>
+#include <boost/endian/conversion.hpp>
+
+enum MessageType {
+    ConnectionRequested = 1,
+    ConnectionAccepted = 2,
+    ConnectionRejected = 3,
+    StubMessage = 4,
+};
+
+class Message {
+private:
+    MessageType messageType;
+    std::vector<uint8_t> data;
+
+public:
+    Message(MessageType _messageType): messageType(_messageType) {
+    }
+
+    Message(MessageType _messageType, std::vector<uint8_t>&& _data) noexcept :
+        messageType(_messageType),
+        data(std::move(_data)) {
+    }
+    
+    Message(Message&& other) noexcept 
+        : messageType(other.messageType), data(std::move(other.data)) {
+        std::cout<< "moving"<< std::endl;
+    }
+
+    MessageType type() {
+        return this->messageType;
+    }
+
+    void write_async_to(boost::asio::ip::tcp::socket& socket, std::function<void(const boost::system::error_code&, std::size_t)> handler) {       
+        auto inputs = prepare_inputs();
+
+        boost::asio::async_write(socket, std::array<boost::asio::const_buffer, 3> {
+            boost::asio::buffer(&std::get<0>(inputs), sizeof(std::get<0>(inputs))),
+            boost::asio::buffer(&std::get<1>(inputs), sizeof(std::get<1>(inputs))),
+            boost::asio::buffer(data.data(), data.size())
+            },[handler](const boost::system::error_code& error, std::size_t bytes_transferred) {
+                handler(error, bytes_transferred);
+            });
+    }
+
+    std::size_t write_to(boost::asio::ip::tcp::socket& socket) {     
+        auto inputs = prepare_inputs();
+
+        return boost::asio::write(socket, std::array<boost::asio::const_buffer, 3> {
+            boost::asio::buffer(&std::get<0>(inputs), sizeof(std::get<0>(inputs))),
+            boost::asio::buffer(&std::get<1>(inputs), sizeof(std::get<1>(inputs))),
+            boost::asio::buffer(data.data(), data.size())
+        });
+    }
+
+    std::tuple<uint32_t, uint32_t> prepare_inputs() {
+        return std::tuple(boost::endian::native_to_big(static_cast<uint32_t>(messageType)),
+                          boost::endian::native_to_big(static_cast<uint32_t>(data.size())));
+    }
+
+    template<typename DataType>
+    friend Message& operator << (Message& msg, const DataType& data) {
+        // Check that the type of the data being pushed is trivially copyable
+        static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
+
+        // Cache current size of vector, as this will be the point we insert the data
+        size_t i = msg.data.size();
+
+        // Resize the vector by the size of the data being pushed
+        msg.data.resize(msg.data.size() + sizeof(DataType));
+
+        // Physically copy the data into the newly allocated vector space
+        std::memcpy(msg.data.data() + i, &data, sizeof(DataType));
+
+        // Return the target message so it can be "chained"
+        return msg;
+    }
+
+    friend Message& operator<<(Message& msg, const std::string& input) {
+        msg.data.assign(input.begin(), input.end());
+        return msg;
+    }
+
+    friend Message& operator>>(Message& msg, std::string& data) {
+        data.assign(msg.data.begin(), msg.data.end());
+        return msg;
+    }	
+};
+
+Message read_from(boost::asio::ip::tcp::socket& socket) {
+    uint32_t messageTypeSerialized = 0;
+    boost::asio::read(socket, boost::asio::buffer(&messageTypeSerialized, sizeof(messageTypeSerialized)));
+    MessageType messageType = static_cast<MessageType>(boost::endian::big_to_native(messageTypeSerialized));
+
+    uint32_t dataLengthSerialized = 0;
+    boost::asio::read(socket, boost::asio::buffer(&dataLengthSerialized, sizeof(dataLengthSerialized)));
+    size_t dataLength = boost::endian::big_to_native(dataLengthSerialized);
+
+    std::vector<uint8_t> data;
+    data.resize(dataLength);
+    boost::asio::read(socket, boost::asio::buffer(data));
+
+    return Message(messageType, std::move(data));
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+use message::Message;
 use rand::Rng;
 
 use std::{
@@ -7,51 +8,54 @@ use std::{
 
 mod message;
 
-fn respond(mut stream: TcpStream) {
-    let mut writing_stream = stream.try_clone().unwrap();
+fn respond(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut writing_stream = stream.try_clone()?;
 
     for message_result in message_iterator(&mut stream) {
-        match message_result {
-            Ok(mut message) => match message.message_type() {
-                message::MessageType::ConnectionRequested => {
-                    let random = rand::thread_rng().gen_range(1..=100);
-                    let mut everyone_is_welcome =
-                        message::Message::new(message::MessageType::ConnectionAccepted);
 
-                    everyone_is_welcome
-                        .push_string(&format!("Welcome! You're our {random} customer today!"))
-                        .write_to(&mut writing_stream)
-                        .unwrap();
-                    writing_stream.flush().unwrap();
-
-                    let mut stub_message = message::Message::new(message::MessageType::StubMessage);
-
-                    stub_message
-                        .push_string(&format!("Yoba, eto ti?"))
-                        .write_to(&mut writing_stream)
-                        .unwrap();
-                    writing_stream.flush().unwrap();
-                }
-                message::MessageType::StubMessage => {
-                    let maybe_request: String = message.pop_string();
-                    println!("stub message got: {maybe_request}");
-
-                    thread::sleep(Duration::from_secs(2));
-
-                    let mut stub_message = message::Message::new(message::MessageType::StubMessage);
-
-                    stub_message
-                        .push_string(&format!("Yoba, eto ti?"))
-                        .write_to(&mut writing_stream)
-                        .unwrap();
-                    writing_stream.flush().unwrap();
-                }
-                _ => todo!(),
-            },
-            Err(err) => {
-                panic!("{err}")
-            }
+        match message_result.and_then(|mut message| process_message(&mut message, &mut writing_stream)) {
+            Ok(_) => continue,
+            err => return err,
         }
+    }
+    Ok(())
+}
+
+fn process_message(message: &mut Message, writing_stream: &mut TcpStream) -> std::io::Result<()> {
+    match message.message_type() {
+        message::MessageType::ConnectionRequested => {
+            let random = rand::thread_rng().gen_range(1..=100);
+            let mut everyone_is_welcome =
+                message::Message::new(message::MessageType::ConnectionAccepted);
+
+            everyone_is_welcome
+                .push_string(&format!("Welcome! You're our {random} customer today!"))
+                .write_to(writing_stream)?;
+            writing_stream.flush()?;
+
+            let mut stub_message = message::Message::new(message::MessageType::StubMessage);
+
+            stub_message
+                .push_string(&format!("Yoba, eto ti?"))
+                .write_to(writing_stream)?;
+            writing_stream.flush()?;
+            Ok(())
+        }
+        message::MessageType::StubMessage => {
+            let maybe_request: String = message.pop_string();
+            println!("stub message got: {maybe_request}");
+
+            thread::sleep(Duration::from_secs(2));
+
+            let mut stub_message = message::Message::new(message::MessageType::StubMessage);
+
+            stub_message
+                .push_string(&format!("Yoba, eto ti?"))
+                .write_to(writing_stream)?;
+            writing_stream.flush()?;
+            Ok(())
+        }
+        _ => todo!(),
     }
 }
 
@@ -66,6 +70,9 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        respond(stream);
+        match respond(stream) {
+            Err(err) => println!("{err:?}"),
+            _ => continue,
+        }
     }
 }
