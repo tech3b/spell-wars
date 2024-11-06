98bdbd6d1d8a6d3a438bbaae56a9c0275cbb4531
l1cache <cache@bk.ru>
2024-11-07 00:05:39 +0100

    very smol improvements. spawn functions extracted

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
-use message::Message;
+use message::{Message, MessageType};
 use rand::Rng;
 
 use std::{
     collections::{HashMap, HashSet, VecDeque},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
-    thread,
+    thread::{self, JoinHandle},
     time::Duration,
 };
 
@@ -14,36 +14,32 @@ mod message;
 fn process_message(
     message: &mut Message,
     user: i32,
-    write_queue_mutex: &Arc<Mutex<VecDeque<Message>>>,
-) -> std::io::Result<()> {
+    write_queue_mutex: Arc<Mutex<VecDeque<Message>>>,
+) -> Result<(), String> {
     match message.message_type() {
-        message::MessageType::ConnectionRequested => {
+        MessageType::ConnectionRequested => {
             let some_client_number: i32 = message.pop().unwrap_or(0);
 
             println!("Some client connected: {some_client_number}");
             println!("Sending client number: {user}");
 
             let mut queue = write_queue_mutex.lock().unwrap();
-            let mut accepted_message =
-                message::Message::new(message::MessageType::ConnectionAccepted);
+            let mut accepted_message = Message::new(MessageType::ConnectionAccepted);
             accepted_message.push(&user);
             queue.push_back(accepted_message);
             queue.push_back(create_stub_message());
 
             Ok(())
         }
-        message::MessageType::StubMessage => {
-            let s2 = message.pop_string().ok_or(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Can't pop string",
-            ))?;
-            let s1 = message.pop_string().ok_or(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Can't pop string",
-            ))?;
+        MessageType::StubMessage => {
+            let s2 = message
+                .pop_string()
+                .ok_or(String::from("Can't pop string"))?;
+            let s1 = message
+                .pop_string()
+                .ok_or(String::from("Can't pop string"))?;
 
-            println!("s1: {s1}");
-            println!("s2: {s2}");
+            println!("message from {user}: s1: {s1}, s2: {s2}");
 
             write_queue_mutex
                 .lock()
@@ -57,7 +53,7 @@ fn process_message(
 }
 
 fn create_stub_message() -> Message {
-    let mut stub_message = message::Message::new(message::MessageType::StubMessage);
+    let mut stub_message = Message::new(MessageType::StubMessage);
     stub_message.push_string("Hello from the other siiiiiiiiiide!");
     stub_message.push_string("At least I can say that I've triiiiiiiiiiied!");
     stub_message
@@ -76,26 +72,11 @@ fn main() {
     ));
     let users = Arc::new(Mutex::new(HashSet::<i32>::new()));
 
-    let users_in_main = users.clone();
-    let user_to_write_deq_in_main = user_to_write_deq.clone();
-    let user_to_read_deq_in_main = user_to_read_deq.clone();
-    thread::spawn(move || loop {
-        println!("new iteration");
-        for user in users_in_main.lock().unwrap().iter() {
-            Option::zip(
-                user_to_read_deq_in_main.lock().unwrap().get(user),
-                user_to_write_deq_in_main.lock().unwrap().get(user),
-            )
-            .and_then(|(read_deq, write_deq)| {
-                read_deq
-                    .lock()
-                    .unwrap()
-                    .pop_front()
-                    .map(|mut message| process_message(&mut message, *user, write_deq).unwrap())
-            });
-        }
-        thread::sleep(Duration::from_secs(2));
-    });
+    spawn_main_thread(
+        users.clone(),
+        user_to_read_deq.clone(),
+        user_to_write_deq.clone(),
+    );
 
     for stream in listener.incoming() {
         let write_deq = Arc::new(Mutex::new(VecDeque::<Message>::new()));
@@ -113,51 +94,20 @@ fn main() {
             .insert(user_id, read_deq.clone());
 
         let actual_stream = stream.unwrap();
-        let mut write_stream = actual_stream.try_clone().unwrap();
-        let mut read_stream = actual_stream.try_clone().unwrap();
 
-        let local_write_deq = write_deq.clone();
-        let local_read_deq = read_deq.clone();
-        let user_to_write_deq_in_write = user_to_write_deq.clone();
-        let user_to_read_deq_in_read = user_to_read_deq.clone();
-
-        let local_write_in_read_deq = write_deq.clone();
-        thread::spawn(move || {
-            loop {
-                match local_write_deq.lock().unwrap().pop_front() {
-                    Some(message) => match message.write_to(&mut write_stream) {
-                        Ok(_) => continue,
-                        Err(_) => break,
-                    },
-                    None => continue,
-                };
-            }
-            user_to_write_deq_in_write
-                .lock()
-                .unwrap()
-                .remove(&user_id)
-                .unwrap();
-            println!("write thread finished for {user_id}");
-        });
-
-        thread::spawn(move || {
-            for message_result in Message::iter(&mut read_stream) {
-                match message_result {
-                    Ok(message) => local_read_deq.lock().unwrap().push_back(message),
-                    Err(_) => break,
-                }
-            }
-            user_to_read_deq_in_read
-                .lock()
-                .unwrap()
-                .remove(&user_id)
-                .unwrap();
-            local_write_in_read_deq
-                .lock()
-                .unwrap()
-                .push_back(Message::new(message::MessageType::ConnectionRejected));
-            println!("read thread finished for {user_id}");
-        });
+        spawn_write_thread(
+            actual_stream.try_clone().unwrap(),
+            user_id,
+            write_deq.clone(),
+            user_to_write_deq.clone(),
+        );
+        spawn_read_thread(
+            actual_stream.try_clone().unwrap(),
+            user_id,
+            read_deq.clone(),
+            user_to_read_deq.clone(),
+            write_deq.clone(),
+        );
     }
 }
 
@@ -171,4 +121,80 @@ fn insert_user(users: &mut HashSet<i32>) -> i32 {
             return random;
         }
     }
+}
+
+fn spawn_write_thread(
+    mut stream: TcpStream,
+    user_id: i32,
+    write_deq: Arc<Mutex<VecDeque<Message>>>,
+    user_to_write_deq: Arc<Mutex<HashMap<i32, Arc<Mutex<VecDeque<Message>>>>>>,
+) -> JoinHandle<()> {
+    thread::spawn(move || {
+        loop {
+            match write_deq.lock().unwrap().pop_front() {
+                Some(message) => match message.write_to(&mut stream) {
+                    Ok(_) => continue,
+                    Err(_) => break,
+                },
+                None => continue,
+            };
+        }
+        user_to_write_deq.lock().unwrap().remove(&user_id).unwrap();
+        println!("write thread finished for {user_id}");
+    })
+}
+
+fn spawn_read_thread(
+    mut stream: TcpStream,
+    user_id: i32,
+    read_deq: Arc<Mutex<VecDeque<Message>>>,
+    user_to_read_deq: Arc<Mutex<HashMap<i32, Arc<Mutex<VecDeque<Message>>>>>>,
+    write_deq: Arc<Mutex<VecDeque<Message>>>,
+) -> JoinHandle<()> {
+    thread::spawn(move || {
+        for message_result in Message::iter(&mut stream) {
+            match message_result {
+                Ok(message) => read_deq.lock().unwrap().push_back(message),
+                Err(_) => break,
+            }
+        }
+        user_to_read_deq.lock().unwrap().remove(&user_id).unwrap();
+        write_deq
+            .lock()
+            .unwrap()
+            .push_back(Message::new(MessageType::ConnectionRejected));
+        println!("read thread finished for {user_id}");
+    })
+}
+
+fn spawn_main_thread(
+    users: Arc<Mutex<HashSet<i32>>>,
+    user_to_read_deq: Arc<Mutex<HashMap<i32, Arc<Mutex<VecDeque<Message>>>>>>,
+    user_to_write_deq: Arc<Mutex<HashMap<i32, Arc<Mutex<VecDeque<Message>>>>>>,
+) {
+    thread::spawn(move || loop {
+        println!("new iteration");
+        for user in users.lock().unwrap().iter() {
+            Option::zip(
+                user_to_read_deq
+                    .lock()
+                    .unwrap()
+                    .get(user)
+                    .map(|a| a.clone()),
+                user_to_write_deq
+                    .lock()
+                    .unwrap()
+                    .get(user)
+                    .map(|a| a.clone()),
+            )
+            .and_then(|(read_deq, write_deq)| {
+                read_deq
+                    .lock()
+                    .unwrap()
+                    .pop_front()
+                    .map(|mut message| process_message(&mut message, *user, write_deq).unwrap())
+            });
+        }
+        thread::sleep(Duration::from_secs(2));
+    });
 }
